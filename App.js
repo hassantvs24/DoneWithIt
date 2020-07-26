5dc792c04ff85c0474293abebd685a3fabf8370c
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-07-27 02:50:00 +0600

    Lanescap Dimension

--- App.js
+++ App.js
@@ -1,27 +1,21 @@
 import { StatusBar as Stbar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, SafeAreaView, Image, Platform, TouchableOpacity, StatusBar } from 'react-native';
+import { StyleSheet, Dimensions, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+import { useDimensions, useDeviceOrientation } from '@react-native-community/hooks';
 
 export default function App() {
 
-  const handlePress = () => console.log("Text Clicked.");
-  const handleImagePress = () => {
-    console.log(StatusBar.currentHeight);
-  };
+  //const handlePress = () => console.log("Text Clicked.");
+
+  //console.log(Dimensions.get('window'));
+  console.log(useDeviceOrientation());
+
+  const {landscape} = useDeviceOrientation();
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text numberOfLines={1} onPress={handlePress}>Hello World Nadu Pappa.</Text>
-      <TouchableOpacity onPress={handleImagePress}>
-        <Image source={{ 
-          width: 200,
-          height: 300,
-          uri : "https://picsum.photos/200/300" 
-          }} />
-      </TouchableOpacity>
-
+      <View style={{ backgroundColor:'dodgerblue', height: landscape ? '100%':'30%', width:'100%' }}></View>
       <Stbar style="auto" />
-      
     </SafeAreaView>
   );
 }
@@ -30,7 +24,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
   },
-});
+}); 
+  

0454869d784ca6c0f5fd0ad586ad7688c7d46409
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-07-27 18:38:47 +0600

    flexShrink

--- App.js
+++ App.js
@@ -10,10 +10,15 @@ export default function App() {
       <View style={{ backgroundColor:'#fff', flex: 1, flexDirection: 'row', 
       justifyContent: 'center', //main
       alignItems: 'center', //secondary
-      flexWrap: 'wrap',
+      //flexWrap: 'wrap',
       alignContent: 'center'
       }}>
-        <View style={{ backgroundColor:'dodgerblue', flexGrow: 1, width: 100, height:100}}></View>
+        <View style={{ 
+          backgroundColor:'dodgerblue', 
+          //flex: 1, 
+          flexShrink: 1,
+          width: 400,
+          height:100}}></View>
         <View style={{ backgroundColor:'gold', width: 100, height:100 }}></View>
         <View style={{ backgroundColor:'tomato', width: 100, height:100 }}></View>
       </View>

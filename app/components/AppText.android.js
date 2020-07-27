d71731be3b01034ce6f3e692f6443ee7745a0709
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-07-28 00:50:28 +0600

    Platform Spasicic by file name

---
+++ app/components/AppText.android.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, StyleSheet } from 'react-native';
+
+const AppText = ({children}) => {
+    return (
+        <Text style={styles.text}>{children}</Text>
+    );
+}
+
+const styles = StyleSheet.create({
+    text: {
+        color: 'blue',
+        fontSize: 20
+    }
+})
+
+export default AppText;

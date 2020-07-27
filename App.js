44eea447ecbea966999d6c7f0cf8422994e8177c
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-07-28 00:22:59 +0600

    Text

--- App.js
+++ App.js
@@ -1,27 +1,22 @@
 import { StatusBar as Stbar } from 'expo-status-bar';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <View style={{ 
-        backgroundColor: "dodgerblue",
-        height: 100,
-        width: 100,
-        padding: 20,
-        }}>
-          <View style={{ 
-            backgroundColor: 'gold',
-            width: 50,
-            height: 50
-            }}>
-
-          </View>
-      </View>
-
+      <Text style={{ 
+        //fontFamily: 'Robot',
+        fontStyle: 'italic',
+        fontWeight: "800",
+        color: 'red',
+        fontSize: 30,
+        textDecorationLine: 'line-through'
+       }} >
+         Hello World
+         </Text>
     </View>
   );
 }

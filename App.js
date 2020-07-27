9fe2ea2643676355522c5beb1758b930824d966c
Hassan Hussain <49582324+hassantvs24@users.noreply.github.com>
2020-07-28 00:06:16 +0600

    Boder Style

--- App.js
+++ App.js
@@ -5,10 +5,21 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import ViewImageScreen from './app/screens/ViewImageScreen';
 
 export default function App() {
-
-
   return (
-      <ViewImageScreen />
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <View style={{ 
+        backgroundColor: "dodgerblue",
+        height: 100,
+        width: 100,
+        borderWidth: 10,
+        borderColor: 'royalblue',
+        borderRadius: 50
+        
+        }}>
+
+      </View>
+
+    </View>
   );
 }
 
